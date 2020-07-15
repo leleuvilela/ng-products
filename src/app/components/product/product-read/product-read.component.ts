8072e1a7fd5ce0cc1e40d1fd64cbef373c5e9ebb
Vinicius Nunes <vnunes@e-deploy.local>
2020-07-15 13:32:24 -0300

    ok

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -1,23 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
-
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-product-read',
   templateUrl: './product-read.component.html',
-  styleUrls: ['./product-read.component.scss']
+  styleUrls: ['./product-read.component.scss'],
 })
 export class ProductReadComponent implements OnInit {
+  products: Product[];
+  displayedColumns = ['id', 'name', 'price', 'action'];
 
-  products: Product[]
-  displayedColumns = ['id', 'name', 'price']
-
-  constructor(private productService: ProductService) { }
+  constructor(
+    private productService: ProductService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
-    this.productService.read().subscribe(products => {
-      this.products = products
-    })
+    this.productService.read().subscribe((products) => {
+      this.products = products;
+    });
   }
 
+  openConfirmDelete(id: string) {
+    const dialogRef = this.dialog.open(ConfirmDelete, { data: { id } });
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) this.deleteProduct(id);
+    });
+  }
+
+  deleteProduct(id: string): void {
+    this.productService.delete(id).subscribe(() => {
+      this.productService.showMessage('Deletado com sucesso!');
+      this.productService.read().subscribe((products) => {
+        this.products = products;
+      });
+    });
+  }
+}
+
+@Component({
+  selector: 'confirm-delete',
+  templateUrl: './confirm-delete.html',
+})
+export class ConfirmDelete {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 }
--- src/app/components/product/product.service.ts
+++ src/app/components/product/product.service.ts
@@ -1,31 +1,66 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from "@angular/material/snack-bar";
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpClient } from '@angular/common/http';
-import { Product } from "./product.model";
-import { Observable } from 'rxjs';
+import { Product } from './product.model';
+import { Observable, EMPTY } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  baseUrl = 'http://localhost:3001/products';
 
-  baseUrl = 'http://localhost:3001/products'
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
-
-  showMessage(msg: string): void {
+  showMessage(msg: string, isError: boolean = false): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
       horizontalPosition: 'right',
-      verticalPosition: 'top'
-    })
+      verticalPosition: 'top',
+      panelClass: isError ? ['msg-error'] : ['msg-success'],
+    });
+  }
+
+  errorHandler(e: any): Observable<any> {
+    this.showMessage('Ops, algo deu errado...', true);
+    return EMPTY;
   }
 
   create(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.baseUrl, product)
+    return this.http.post<Product>(this.baseUrl, product).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 
   read(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.baseUrl)
+    return this.http.get<Product[]>(this.baseUrl).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  readById(id: string): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/${id}`).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
+  }
+
+  update(product: Product): Observable<Product> {
+    return this.http
+      .put<Product>(`${this.baseUrl}/${product.id}`, product)
+      .pipe(
+        map((obj) => obj),
+        catchError((e) => this.errorHandler(e))
+      );
+  }
+
+  delete(id: string): Observable<Product> {
+    return this.http.delete<Product>(`${this.baseUrl}/${id}`).pipe(
+      map((obj) => obj),
+      catchError((e) => this.errorHandler(e))
+    );
   }
 }
